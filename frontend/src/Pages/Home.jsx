40a2baf88a9b9ef0f9313b4f30997b85b7d2fa64
marelign <yimermarelign@gmail.com>
2026-04-17 10:14:36 +0300

    fff

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from '../components/home/Hero';
-import About from '../components/home/About';
-import Services from '../components/home/Services';
-import Portfolio from '../components/home/Portfolio';
-import Features from '../components/home/Features';
-import Testimonials from '../components/home/Testimonials';
-import Contact from '../components/home/Contact';
+import Hero from '../Components/home/Hero';
+import About from '../Components/home/About';
+import Services from '../Components/home/Services';
+import Portfolio from '../Components/home/Portfolio';
+import Features from '../Components/home/Features';
+import Testimonials from '../Components/home/Testimonials';
+import Contact from '../Components/home/Contact';
 
 const Home = () => {
     return (
